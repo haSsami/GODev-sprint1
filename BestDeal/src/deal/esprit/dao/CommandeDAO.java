99f8f500d16417fb16fe3d825d87aef5c836e058
wiss75 <bchiniwassim75@gmail.com>
2014-02-27 00:01:22 +0100

    categorie,produit,commande et panier

--- BestDeal/src/deal/esprit/dao/CommandeDAO.java
+++ BestDeal/src/deal/esprit/dao/CommandeDAO.java
@@ -10,6 +10,7 @@ package deal.esprit.dao;
  *
  * @author wassim
  */
+import deal.esprit.entities.Categorie;
 import deal.esprit.entities.Commande;
 import deal.esprit.util.MyConnection;
 import java.sql.PreparedStatement;
@@ -33,4 +34,32 @@ public class CommandeDAO {
         } catch (SQLException ex) {
             System.out.println("erreur lors de l'insertion "+ex.getMessage());}   
     }
-   }
+    //Mise a jour de la quantite de commande
+        public void updateCommande(Commande c) throws SQLException{
+        String req="select id_commande from commande where (id_client="+c.getClient().getIdClient()+" AND id_produit="+c.getProduit().getId_produit()+");";
+        int id=0;
+        try {
+        
+         Statement statement = MyConnection.getInstance().createStatement();
+         ResultSet resultat = statement.executeQuery(req);
+         id=resultat.getInt(1);
+        }
+        catch (SQLException ex) {
+            System.out.println("erreur id "+ex.getMessage());
+        }
+        
+        String requete = "update commande set qte=? where id_commande=?";
+        try {
+            PreparedStatement ps = MyConnection.getInstance().prepareStatement(requete);
+            ps.setInt(1, c.qte);
+            ps.setInt(2,id);
+           
+            ps.executeUpdate();
+            System.out.println("Mise à jour effectuée avec succès");
+        } catch (SQLException ex) {
+            System.out.println("erreur lors de la mise à jour "+ex.getMessage());
+        }
+
+    }
+  
+}
