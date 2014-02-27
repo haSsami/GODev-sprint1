e388aba1704914d0eba5c29606209e4d1a84c667
wiss75 <bchiniwassim75@gmail.com>
2014-02-27 17:37:11 +0100

    Merge origin/master

--- BestDeal/src/deal/esprit/dao/ProduitDAO.java
+++ BestDeal/src/deal/esprit/dao/ProduitDAO.java
@@ -16,18 +16,18 @@ import java.sql.SQLException;
  * @author wassim
  */
 public class ProduitDAO {
-    public void insertProduit(Produit p)
-    {
-     String requete = "insert into produit (nom_produit,logo,prix_primaire,qte_stock,id_categorie) values (?,?,?,?,?)";
+   /* public void insertProduit(Produit p)
+     {
+    String requete = "insert into produit (nom_produit,logo,prix_primaire,qte_stock,id_categorie) values (?,?,?,?,?)";
         try {
             PreparedStatement ps = MyConnection.getInstance().prepareStatement(requete);
-            ps.setInt(1, c.produit.getId_produit());
-            ps.setInt(2, c.client.getIdClient());
-            ps.setInt(3, c.qte);          
+            ps.setInt(1, p.getId_produit());
+            ps.setInt(2, p.);
+            ps.setInt(3, p.getQte_stock());          
             ps.executeUpdate();
-            System.out.println("Ajout effectuée avec succès");
+            System.out.println("Ajout effectuÃ©e avec succÃ¨s");
         } catch (SQLException ex) {
             System.out.println("erreur lors de l'insertion "+ex.getMessage());}   
     }
-    
+    */
 }
